61acf82c6aa99ce6021328fab7d1a802a9010843
0xtsukino <0xtsukino@gmail.com>
2022-12-11 15:15:16 -0800

    force dependency

--- scaffold.js
+++ scaffold.js
@@ -1,2 +1,17 @@
 // for backwards compat
-module.exports = require('json-rpc-engine/src/createScaffoldMiddleware')
+module.exports = function createScaffoldMiddleware (handlers) {
+  return (req, res, next, end) => {
+    const handler = handlers[req.method]
+    // if no handler, return
+    if (handler === undefined) {
+      return next()
+    }
+    // if handler is fn, call as middleware
+    if (typeof handler === 'function') {
+      return handler(req, res, next, end)
+    }
+    // if handler is some other value, use as result
+    res.result = handler
+    return end()
+  }
+}
